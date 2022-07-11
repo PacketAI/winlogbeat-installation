813e71291caf05ec260b9b6bae6b8a77d4192f1f
Yuchao QIAN <yuchao@packetai.co>
2022-07-11 12:23:00 +0200

    fix script

--- install.ps1
+++ install.ps1
@@ -1,3 +1,4 @@
+param ($clustername, $infra, $token)
 $ProgressPreference = 'SilentlyContinue'
 curl -o wlb.zip  https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-8.3.2-windows-x86_64.zip
 Expand-Archive .\wlb.zip
@@ -6,7 +7,6 @@ rm -r .\wlb
 rm .\wlb.zip
 cd .\winlogbeat
 curl -o winlogbeat.yaml "https://raw.githubusercontent.com/PacketAI/winlogbeat-installation/main/example.config.wlb.yaml"
-param ($clustername, $infra, $token)
-(Get-Content winlogbeat.yaml) -Replace '${X-PAI-IID}', $infra | Set-Content winlogbeat.yaml
-(Get-Content winlogbeat.yaml) -Replace '${CLUSTER_NAME}', $clustername | Set-Content winlogbeat.yaml
-(Get-Content winlogbeat.yaml) -Replace '${X-PAI-TOKEN}', $token | Set-Content winlogbeat.yaml
+(Get-Content winlogbeat.yaml) -Replace '\$\{X-PAI-IID\}', $infra | Set-Content winlogbeat.yaml
+(Get-Content winlogbeat.yaml) -Replace '\$\{CLUSTER_NAME\}', $clustername | Set-Content winlogbeat.yaml
+(Get-Content winlogbeat.yaml) -Replace '\$\{X-PAI-TOKEN\}', $token | Set-Content winlogbeat.yaml
